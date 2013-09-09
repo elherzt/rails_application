39d18551adb2269a40ef8102e53e18b0eaf8da52
Jesus Romero <jose.romero@tangosource.com>
2013-09-09 16:51:30 -0500

    passed all test to create projects with validations

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,15 +6,18 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+
   def create
     @project = Project.new(project_params)
     if @project.save
       flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      # nothing, yet
+      flash[:alert] = "Project has not been created."
+      render :action => "new"
     end
   end
+
   def show
     @project = Project.find(params[:id])
   end
